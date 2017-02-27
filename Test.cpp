5270097fce83fe2f74a82a9d73c5800c6530b139
Turupawn <ahmed.hn.43@gmail.com>
2017-02-27 17:02:42 -0600

    moar tests

--- Test.cpp
+++ Test.cpp
@@ -11,12 +11,25 @@ void test()
 
     map<char,string> res = getHuffman(vec, vec2);
 
+
+    static const char arrx[] = {'x', 'y', 'z'};
+    vector<char> vecx (arrx, arrx + sizeof(arrx) / sizeof(arrx[0]) );
+
+    static const int arr2x[] = {5, 10, 15,};
+    vector<int> vec2x (arr2x, arr2x + sizeof(arr2x) / sizeof(arr2x[0]) );
+
+    map<char,string> resx = getHuffman(vecx, vec2x);
+
     if(res['f'] == "0"
        && res['c'] == "100"
        && res['d'] == "101"
        && res['a'] == "1100"
        && res['b'] == "1101"
        && res['e'] == "111"
+
+       && resx['x'] == "10"
+       && resx['y'] == "11"
+       && resx['z'] == "0"
        )
     {
         cout<<"Test: Pass"<<endl;
